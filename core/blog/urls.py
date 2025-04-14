75d6f87741464ba30caa49233d3dfabf3547c3d5
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-14 14:59:44 +0330

    04.03

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from . import views
 
 app_name = "blog"
 
 urlpatterns = [
-
+    path("fbv-index/", views.indexView, name='fbv-index'),
+    path("cbv-index/", TemplateView.as_view(template_name='index.html', extra_context={"name": "class base view"}), name='cbv-index'),
+    path("cbv-index-custum/", views.IndexTemplateView.as_view(), name='cbv-index-custum'),
+    path('go-to-itmeter/', RedirectView.as_view(url='https://itmeter.ir/'), name='go-to-itmeter'),
+    path('go-to-cbv-index/', RedirectView.as_view(pattern_name="blog:cbv-index"), name='go-to-cbv-index')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Post
+
+
+def indexView(request):
+    """
+    a function based view to show index page
+    """
+    context = {"name": 'function base view'}
+    return render(request, 'index.html', context)
+
+
+
+class IndexTemplateView(TemplateView):
+    """
+    a class based view to show index page
+    """
+    template_name = 'index.html'
+    
+    def get_context_data(self, **kwargs):
+        # Call (TemplateView) the superclass' method to get_context_data for creating an object which is named context
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'custum class-based view'
+        context['post'] = Post.objects.all()
+        return context
