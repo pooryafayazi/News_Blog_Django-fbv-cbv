5ca39d421b969dbcdd53ab556236ca69a8692257
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-15 13:58:46 +0330

    05.10

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework import serializers
 
+from ...models import Post
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+
+
+
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = ["id", "status", "image", "title", "content", "created_date", "updated_date"]
+
+
+
+
