e8ee09383522e404c6a740c0c9acc090e43419bc
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-15 17:48:57 +0330

    06.16

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from ...models import Post, Category
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
 
 
 # class PostSerializer(serializers.Serializer):
@@ -15,7 +20,6 @@ class PostSerializer(serializers.ModelSerializer):
     snippet = serializers.ReadOnlyField(source='get_snippet')
     relative_url = serializers.URLField(source='get_absolute_api_url')
     absolute_url = serializers.SerializerMethodField()
-    category = serializers.SlugRelatedField(many=False, slug_field='name', queryset=Category.objects.all())
     class Meta:
         model = Post
         fields = ["id", "status", "image", "title", "content", "snippet", "category", "relative_url", "absolute_url", "created_date", "updated_date"]
@@ -24,10 +28,10 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
         
-class CategorySerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Category
-        fields = ["id", "name"]
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['category'] = CategorySerializer(instance.category).data
+        return rep
+
 
     
