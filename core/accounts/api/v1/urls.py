4610bb9dd60d65e1381e310036111739841dbc11
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-04-16 16:05:12 +0330

    08.10

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import ObtainAuthToken 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from . import views
 
@@ -10,7 +11,13 @@ urlpatterns = [
     path('registration/', views.RegistrationAPIView.as_view(), name='registration'),
     path('token/login/', ObtainAuthToken.as_view(), name='token-login'),
     path('token-custom/login/', views.CustomObtainAuthToken.as_view(), name='token-custom-login'),
-    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout')
+    path('token/logout/', views.CustomDiscardAuthToken.as_view(), name='token-logout'),
+    
+    # simple jwt
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt-token-create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt-token-refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-token-verify'),
+    
     
     # change password
     
--- core/core/settings.py
+++ core/core/settings.py
@@ -184,6 +184,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
     # coreapi settings
     # "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
